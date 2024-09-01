d0dcf2f3bb164c6169957b03fd880308f9ee63d0
AInyatkin <link_alexsey@mail.ru>
2024-09-01 15:17:38 +0700

    create task page

--- app/tasks/create-task/page.tsx
+++ app/tasks/create-task/page.tsx
@@ -1,10 +1,23 @@
+"use client"
+
+
 import classNames from "classnames";
 import styles from "./page.module.scss";
 import Link from "next/link";
 import Image from "next/image";
+import Button from "@/app/_components/ui/Button/Button";
+import Input from "@/app/_components/ui/Input/Input";
+import SelectSearch from "react-select-search";
 
 
 export default function CreateTaskPage() {
+
+    const templates = ["Веб-разработка", "Мобильная разработка", "Искусственный интеллект", "Базы данных", "Безопасность информации"];
+    const categories = [
+        { value: "1", name: "Наука и техника" },
+        { value: "2", name: "Информационные технологии" },
+    ];
+
     return (
         <div className={classNames(styles.container, "container")}>
             <div className={styles.taskHeader}>
@@ -15,7 +28,61 @@ export default function CreateTaskPage() {
                 <p className="title">
                     Создать задание
                 </p>
+            </div>
+            <div className={styles.taskContent}>
+                <div className={styles.templates}>
+                    <p className="text fw500">Шаблоны технических заданий</p>
+                    {templates.map((template, index) => (
+                        <Button key={index} type="primary" className={styles.templateButton}>
+                            {template}
+                        </Button>
+                    ))}
+                </div>
+                <div className={styles.createTask}>
+                    <p className="text fw500">Опишите, что нужно сделать</p>
+                    <p className="text gray fz20">Разместите свою задачу на платформе. Ваша задача станет видимой для тысячи студентов, и некоторые из них откликнуться на ваше задание уже с готовым решением. Изучите их решения и выберите лучших из них. Подтвердите заказ, когда будете удовлетворены результатом на 100% и оставьте рецензию на их решение.</p>
+                    <div className={styles.taskForm}>
+                        <p className="text">Название задания</p>
+                        <Input type="text" placeholder="Название должно привлечь внимание и отразить суть задачи." containerClassName={styles.textName} />
+                        <p className="text">О задании</p>
+                        <Input type="textarea" placeholder="Опишите что именно вам нужно. Включите в описание важные аспекты." containerClassName={styles.textarea} />
+                        <div className={styles.underdescription}>
+                            <p className="text gray fz20">Файл</p>
+                            <p className={classNames("text gray fz20", styles.length)}>0 из 2000 символов (минимум 150)</p>
+                        </div>
+                        <div className={styles.selects}>
+                            <div className={styles.select}>
+                                <p className="text fw500">Категории</p>
+                                <SelectSearch
+                                    options={categories}
+                                    placeholder="Выберите категорию"
+                                />
+                            </div>
+                            <div className={styles.select}>
+                                <p className="text fw500">Сроки</p>
+                                <SelectSearch
+                                    options={[
+                                        { value: "1", name: "1 неделя" },
+                                        { value: "2", name: "2 недели" },
+                                        { value: "3", name: "3 недели" },
+                                        { value: "4", name: "4 недели" },
+                                        { value: "5", name: "5 недель" },
+                                    ]}
+                                    placeholder="Выберите сроки"
+                                />
+                            </div>
+                        </div>
+                        <div className={styles.submitButtons}>
+                            <Button type="secondary" className={styles.submitButton}>
+                                Демо
+                            </Button>
+                            <Button type="primary" className={styles.submitButton}>
+                                Сохранить как PDF
+                            </Button>
+                        </div>
 
+                    </div>
+                </div>
             </div>
         </div>
     );
