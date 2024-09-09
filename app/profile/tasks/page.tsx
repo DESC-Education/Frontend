326802c9fe2865faad9eb72903d0db3ac247f389
AInyatkin <link_alexsey@mail.ru>
2024-09-09 21:27:14 +0700

    task create done

--- app/profile/tasks/page.tsx
+++ app/profile/tasks/page.tsx
@@ -1,68 +1,110 @@
 "use client";
 
-import { date } from "zod";
+import { act, createRef, ReactNode, RefObject, useState } from "react";
 import styles from "./page.module.scss";
-import { useTypesSelector } from "@/app/_hooks/useTypesSelector";
+import Button from "../../_components/ui/Button/Button";
+import classNames from "classnames";
 import Link from "next/link";
-import Button from "@/app/_components/ui/Button/Button";
-import { ICompanyProfile, ITask } from "@/app/_types";
-import TaskCard from "@/app/_components/TaskCard/TaskCard";
-import ProfileStatus from "../ProfileStatus/ProfileStatus";
-import { ProfileRoute } from "@/app/_utils/protectedRoutes";
 
-const tasks: ITask[] = [
-    {
-        name: "Заказ1",
-        deadline: "10.03.2024",
-        description: "asdasd",
-        id: "1",
-        company: {
-            id: "1",
-            logoImg: {
-                id: "2",
-                name: "logo1",
-                path: "/images/userImage2.png",
-                type: "png",
-            },
-            linkToCompany: "",
-            description: "",
-            phone: "",
-            emailVisibility: false,
-            phoneVisibility: false,
-            timezone: 0,
-            city: {
-                id: "1",
-                name: "Moscow",
-                region: "Россия",
-            },
-            skills: [],
-            companyName: "",
-            firstName: "",
-            lastName: "",
-            verification: { status: "verified" },
-        },
-        isVisible: true,
-        createdAt: "10.03.2024",
-        files: [],
-        isSuspicious: false,
-        isVerified: true,
-    },
-];
+type TasksPage = "current" | "past";
 
-export default function Home() {
-    const { profileVerification } = useTypesSelector(
-        (state) => state.userReducer,
-    );
+export default function JobsPage() {
+    const [activeTab, setActiveTab] = useState<TasksPage>("current");
+    const [isAnimating, setIsAnimating] = useState<boolean>(false);
 
-    if (profileVerification.status !== "verified") {
-        return <ProfileStatus profileVerification={profileVerification} />;
-    }
+    const handleTabChange = (newTab: TasksPage) => {
+        if (activeTab === newTab) return;
+
+        setIsAnimating(true);
+        setTimeout(() => {
+            setActiveTab(newTab);
+            setIsAnimating(false);
+        }, 300);
+    };
+
+    const tasks = [];
+
+    const getJobsPageContent = (
+        jobsPage: TasksPage,
+    ): {
+        content: ReactNode;
+        ref: RefObject<HTMLDivElement>;
+    } => {
+        switch (jobsPage) {
+            case "current":
+                return {
+                    content: (
+                        <div className={styles.content}>
+                            {tasks.length > 0 ? (
+                                <div>Такси есть</div>
+                            ) : (
+                                <div className={styles.noTasks}>
+                                    <img
+                                        src="/images/no-tasks.png"
+                                        alt="no tasks"
+                                    />
+                                    <p className="title">Заданий нет</p>
+                                    <Link href="/create-task">
+                                        <Button type="secondary">
+                                            Создать задание
+                                        </Button>
+                                    </Link>
+                                </div>
+                            )}
+                        </div>
+                    ),
+                    ref: createRef(),
+                };
+            case "past":
+                return {
+                    content: (
+                        <div className={styles.content}>
+                            {tasks.length > 0 ? (
+                                <div>Такси есть</div>
+                            ) : (
+                                <div className={styles.noTasks}>
+                                    <img
+                                        src="/images/no-tasks.png"
+                                        alt="no tasks"
+                                    />
+                                    <p className="title">Заданий нет</p>
+                                </div>
+                            )}
+                        </div>
+                    ),
+                    ref: createRef(),
+                };
+        }
+    };
 
     return (
-        <div className={styles.container}>
-            {tasks.map((task, index) => (
-                <TaskCard key={index} task={task} />
-            ))}
+        <div className={classNames(styles.container, "container")}>
+            <div className={styles.navigation}>
+                <p className="title fz48"> Мои задания</p>
+                <div className={styles.navigationButtons}>
+                    <Button
+                        className={styles.navigationButton}
+                        type={activeTab === "current" ? "secondary" : "primary"}
+                        onClick={() => handleTabChange("current")}
+                    >
+                        Актуальные
+                    </Button>
+                    <Button
+                        className={styles.navigationButton}
+                        type={activeTab === "past" ? "secondary" : "primary"}
+                        onClick={() => handleTabChange("past")}
+                    >
+                        Архив
+                    </Button>
+                </div>
+            </div>
+            <div
+                className={classNames(styles.content, {
+                    [styles.exit]: isAnimating,
+                })}
+            >
+                {getJobsPageContent(activeTab).content}
+            </div>
         </div>
     );
 }
