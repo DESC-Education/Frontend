159e70d23b51060c1c39d42e92b70931f0b43c02
LuckyFrog-html <bogvanya.04@mail.ru>
2024-09-14 19:35:27 +0700

    fix mobile v2

--- app/layout.tsx
+++ app/layout.tsx
@@ -46,8 +46,6 @@ const gilroy = localFont({
 export const viewport: Viewport = {
     width: "device-width",
     initialScale: 1,
-    maximumScale: 1,
-    userScalable: false,
 };
 
 export const metadata: Metadata = {
