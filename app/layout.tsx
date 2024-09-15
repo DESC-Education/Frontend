72991d0183951387498bbd7b1c2b67f834b42ae5
LuckyFrog-html <bogvanya.04@mail.ru>
2024-09-15 23:02:44 +0700

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,6 +17,7 @@ import Footer from "./_components/Footer/Footer";
 import Head from "next/head";
 
 import hummingbird from "../public/images/hummingbird.jpg";
+import Script from "next/script";
 
 const gilroy = localFont({
     src: [
@@ -100,6 +101,21 @@ export default function RootLayout({
     return (
         <html lang="en">
             <body className={gilroy.className}>
+                <Script id="metrika-counter" strategy="afterInteractive">
+                    {`(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+              m[i].l=1*new Date();
+              for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+              k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+              (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+ 
+              ym(98344007, "init", {
+                    defer: true,
+                    clickmap:true,
+                    trackLinks:true,
+                    accurateTrackBounce:true,
+                    webvisor:true
+              });`}
+                </Script>
                 <Providers>
                     <ClientRootLayout>
                         <>
