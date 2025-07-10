f1558725db528ddfd2b4da551d4bf379a5fc844f
Chyngyz <61840750+chyngyz475@users.noreply.github.com>
2025-07-10 18:40:38 +0300

    1

--- app/_store/reducers/index.ts
+++ app/_store/reducers/index.ts
@@ -4,7 +4,7 @@ import contentReducer from "./contentSlice";
 import taskReducer from "./taskSlice";
 import profileVerifyReducer from "./profileVerifySlice";
 import chatReducer from "./chatSlice";
-import vacancyReducer from "./vacancySlice";
+// import vacancyReducer from "./vacancySlice";
 import { enableMapSet } from "immer";
 import { profile } from "console";
 
@@ -16,7 +16,7 @@ export const rootReducer = combineReducers({
     chatReducer,
     taskReducer,
     profileVerifyReducer,
-    vacancyReducer,
+    // vacancyReducer,
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
